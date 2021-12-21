38c3d28ae718f76b0aec337e61187a08e0e65727
GameHole <741419087@qq.com>
2021-12-21 11:32:03 +0800

    添加发布命令

---
+++ publish-VivoAdSDK.bat
@@ -0,0 +1,4 @@
+E:
+cd E:\WorkSpace\unityProjects\Gits\VIVOAdSDK\Assets\VivoAdSDK
+npm publish --registry http://172.16.1.171:4873/
+pause
---
+++ publish-VivoNativeAd.bat
@@ -0,0 +1,3 @@
+E:
+cd E:\WorkSpace\unityProjects\Gits\VIVOAdSDK\Assets\VivoNativeAd
+npm publish --registry http://172.16.1.171:4873/
